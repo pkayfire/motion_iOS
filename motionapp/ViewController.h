32d3fd617e6969701fb0b755b3c998030c2f568e
Peter Kim <peterkim45366@gmail.com>
2014-11-08 15:24:42 -0800

    Initial Commit

---
+++ motionapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  motionapp
+//
+//  Created by Peter Kim on 11/8/14.
+//  Copyright (c) 2014 Motion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ motionapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  motionapp
+//
+//  Created by Peter Kim on 11/8/14.
+//  Copyright (c) 2014 Motion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
