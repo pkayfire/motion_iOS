54c77ce807bb27e370201f4d31fa797cbedcca38
Peter Kim <peterkim45366@gmail.com>
2014-11-09 23:17:51 -0800

    adding MNPlayerViewController

---
+++ motionapp/MNPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MNPlayerViewController.h
+//  motionapp
+//
+//  Created by Peter Kim on 11/9/14.
+//  Copyright (c) 2014 Motion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNPlayerViewController : UIViewController
+
+@end
