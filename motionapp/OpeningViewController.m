6193e96ac1d337a5b308e4d3184574238008d471
Peter Kim <peterkim45366@gmail.com>
2014-11-09 16:13:47 -0800

    added app icon

--- motionapp/OpeningViewController.m
+++ motionapp/OpeningViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UIColor *motionRed = [UIColor colorWithRed:245.0/255.0f green:110.0/255.0f blue:94.0/255.0f alpha:1.0f];
+    
     FLAnimatedImage *backgroundImage = [[FLAnimatedImage alloc] initWithAnimatedGIFData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"subway_ios_2" ofType:@"gif"]]];
     self.backgroundImageView.animatedImage = backgroundImage;
     
@@ -26,7 +28,7 @@
     
     self.signUpButton.titleLabel.font = [UIFont fontWithName:@"VarelaRound" size:21.0f];
     [self.signUpButton.layer setBorderWidth:2.5f];
-    [self.signUpButton.layer setBorderColor:[[UIColor whiteColor] CGColor]];
+    [self.signUpButton.layer setBorderColor:[motionRed CGColor]];
     [self.signUpButton.layer setCornerRadius:5.0f];
 }
 
